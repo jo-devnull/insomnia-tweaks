5a1d85e121ec729ddbdb45c570c403589f5aa682
Jorge Pereira <jorge.devnull@gmail.com>
2025-06-20 16:14:30 -0300

    add compat between In Control and Lunar

--- src/main/java/com/github/dylanxyz/insomnia/Insomnia.java
+++ src/main/java/com/github/dylanxyz/insomnia/Insomnia.java
@@ -1,7 +1,13 @@
 package com.github.dylanxyz.insomnia;
 
 import com.mojang.logging.LogUtils;
+import com.mrbysco.lunar.LunarPhaseData;
+import com.mrbysco.lunar.api.ILunarEvent;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.slf4j.Logger;
 
@@ -14,5 +20,14 @@ public class Insomnia
     public Insomnia()
     {
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @Mod.EventBusSubscriber
+    public static class LevelTickHandler
+    {
+        @SubscribeEvent
+        public static void onLevelTick(TickEvent.LevelTickEvent event) {
+            LunarCompat.onWorldTick(event);
+        }
     }
 }
---
+++ src/main/java/com/github/dylanxyz/insomnia/LunarCompat.java
@@ -0,0 +1,51 @@
+package com.github.dylanxyz.insomnia;
+
+import com.mrbysco.lunar.LunarPhaseData;
+import com.mrbysco.lunar.api.ILunarEvent;
+import mcjty.incontrol.data.DataStorage;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraftforge.event.TickEvent;
+
+import javax.xml.crypto.Data;
+
+public class LunarCompat
+{
+    private static boolean checked = false;
+
+    public static void onWorldTick(TickEvent.LevelTickEvent event) {
+        if (event.phase == TickEvent.Phase.END && event.level instanceof ServerLevel world) {
+            int time = (int) (world.getDayTime() % 24000L);
+
+            if (time > 0 && time < 13000) {
+                if (checked) {
+                    clearLunarPhases(world);
+                }
+
+                checked = false;
+            }
+
+            if (!checked && time > 13000 && time < 23000) {
+                LunarPhaseData phaseData = LunarPhaseData.get(world);
+                ILunarEvent lunarEvent = phaseData.getActiveLunarEvent();
+
+                if (phaseData.hasEventActive() && lunarEvent != null) {
+                    checked = true;
+                    String moonPhase = lunarEvent.getID().getNamespace() + ":" + lunarEvent.getID().getPath();
+                    DataStorage.getData(world).setPhase(moonPhase, true);
+                    Insomnia.LOGGER.info("Enabling moon phase: {}", moonPhase);
+                }
+            }
+        }
+    }
+
+    public static void clearLunarPhases(ServerLevel world) {
+        DataStorage icData = DataStorage.getData(world);
+
+        for (String phase : icData.getPhases()) {
+            if (phase.startsWith("lunar:")) {
+                DataStorage.getData(world).setPhase(phase, false);
+                Insomnia.LOGGER.info("Disabling moon phase: {}", phase);
+            }
+        }
+    }
+}
