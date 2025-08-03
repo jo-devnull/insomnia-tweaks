a9c72a33e0ace49a1836efa97f93f1134184061e
jo-devnull <jorge.devnull@gmail.com>
2025-08-03 20:02:22 -0300

    fix nehemoth mixin

--- src/main/java/com/github/dylanxyz/insomnia/mixin/fromtheshadows/MixinNehemothEntity.java
+++ src/main/java/com/github/dylanxyz/insomnia/mixin/fromtheshadows/MixinNehemothEntity.java
@@ -1,44 +1,31 @@
 package com.github.dylanxyz.insomnia.mixin.fromtheshadows;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
 import net.minecraft.tags.BiomeTags;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobSpawnType;
-import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.ServerLevelAccessor;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.Biomes;
 import net.sonmok14.fromtheshadows.server.entity.mob.NehemothEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = NehemothEntity.class, remap = false)
 public abstract class MixinNehemothEntity
 {
-    @Shadow
-    private static boolean isNether(ServerLevelAccessor worldIn, BlockPos position) {
-        return false;
-    }
+    @Inject(at = @At("HEAD"), method = "canNehemothSpawn", cancellable = true)
+    private static void insomnia$canNehemothSpawn(EntityType<NehemothEntity> entityType, ServerLevelAccessor iServerWorld, MobSpawnType reason, BlockPos pos, RandomSource random, CallbackInfoReturnable<Boolean> cir) {
+        Holder<Biome> biome = iServerWorld.getBiome(pos);
 
-    @Shadow
-    private static boolean isBiomeSoulSandValley(LevelAccessor worldIn, BlockPos position) {
-        return false;
-    }
-
-    @Shadow
-    public static boolean checkMonsterSpawnRules(EntityType<? extends Monster> p_33018_, ServerLevelAccessor p_33019_, MobSpawnType p_33020_, BlockPos p_33021_, RandomSource p_33022_) {
-        return false;
-    }
-
-    /**
-     * @author jo-devnull
-     * @reason Fix nehemoth spawning
-     */
-    @Overwrite
-    public static <T extends Mob> boolean canNehemothSpawn(EntityType<NehemothEntity> entityType, ServerLevelAccessor iServerWorld, MobSpawnType reason, BlockPos pos, RandomSource random) {
-        return reason == MobSpawnType.SPAWNER || iServerWorld.getBiome(pos).is(BiomeTags.IS_OVERWORLD) || (isNether(iServerWorld, pos) && isBiomeSoulSandValley(iServerWorld, pos)) && checkMonsterSpawnRules(entityType, iServerWorld, reason, pos, random);
+        cir.setReturnValue(
+            reason == MobSpawnType.SPAWNER || biome.is(BiomeTags.IS_OVERWORLD) ||
+            (biome.is(BiomeTags.IS_NETHER) && biome.is(Biomes.SOUL_SAND_VALLEY)) &&
+            NehemothEntity.checkMonsterSpawnRules(entityType, iServerWorld, reason, pos, random)
+        );
     }
 }
