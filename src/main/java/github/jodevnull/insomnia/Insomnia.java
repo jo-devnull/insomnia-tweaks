6fd4b32879d6c0387bfa6518ac180db5ba158f8d
jo-devnull <jorge.devnull@gmail.com>
2026-05-11 08:23:53 -0300

    update some things

--- src/main/java/github/jodevnull/insomnia/Insomnia.java
+++ src/main/java/github/jodevnull/insomnia/Insomnia.java
@@ -32,6 +32,6 @@ public class Insomnia
     }
 
     private void registerCommand(RegisterCommandsEvent event) {
-        MiscCommands.createCommand(event.getDispatcher());
+        MiscCommands.registerCommands(event.getDispatcher());
     }
 }
--- src/main/java/github/jodevnull/insomnia/cmds/MiscCommands.java
+++ src/main/java/github/jodevnull/insomnia/cmds/MiscCommands.java
@@ -1,21 +1,45 @@
 package github.jodevnull.insomnia.cmds;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import github.jodevnull.insomnia.Insomnia;
+import github.jodevnull.insomnia.mixin.quests.FTBQuestsCommandsAccessor;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.world.entity.player.Player;
 
 public class MiscCommands
 {
-    public static void createCommand(CommandDispatcher<CommandSourceStack> dispatcher)
+    public static void registerCommands(CommandDispatcher<CommandSourceStack> dispatcher)
     {
-        dispatcher.register(Commands.literal(Insomnia.MODID)
-            .then(Commands.literal("sleep").executes(MiscCommands::sleepCommand)));
+        final var root = Commands.literal(Insomnia.MODID);
+
+        sleepCommand(dispatcher);
+        addQuestCommand(root);
+
+        dispatcher.register(root);
     }
 
-    private static int sleepCommand(CommandContext<CommandSourceStack> context) {
+    public static void addQuestCommand(LiteralArgumentBuilder<CommandSourceStack> root) {
+        root.then(
+            Commands.literal("open_quest").then(
+            Commands.argument("player", EntityArgument.player()).then(
+            Commands.argument("questId", StringArgumentType.string()).executes(context -> {
+                final var player = EntityArgument.getPlayer(context, "player");
+                final var questId = StringArgumentType.getString(context, "questId");
+                return FTBQuestsCommandsAccessor.openQuest(player, questId);
+            })))
+        );
+    }
+
+    private static void sleepCommand(CommandDispatcher<CommandSourceStack> dispatcher) {
+        dispatcher.register(Commands.literal("sleep").executes(MiscCommands::onSleep));
+    }
+
+    private static int onSleep(CommandContext<CommandSourceStack> context) {
         Player player = context.getSource().getPlayer();
 
         if (player == null)
